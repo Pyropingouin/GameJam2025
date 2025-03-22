49d9709f14729258c37076a34ae6252d0a0bb236
P4tofe <siletourneau@cablevision.qc.ca>
2025-03-21 22:37:03 -0400

    Highlight cards

--- gamejam2025/Scripts/card.gd
+++ gamejam2025/Scripts/card.gd
@@ -1,11 +1,21 @@
 extends Node2D
 
+signal hovered
+signal hovered_off
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	# !! Toutes les cartes doivent être enfant de CardManager !!
+	get_parent().connect_card_signals(self)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_area_2d_mouse_entered() -> void:
+	print("1")
+	emit_signal("hovered", self)
+
+func _on_area_2d_mouse_exited() -> void:
+	print("2")
+	emit_signal("hovered_off", self)
