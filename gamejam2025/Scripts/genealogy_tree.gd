30d7ef31379908f928a7216117880ccef5298f1c
Cédrik Lampron <cedrik.lampron@usherbrooke.ca>
2025-03-22 11:09:09 -0400

    avatar + name popup

--- gamejam2025/Scripts/genealogy_tree.gd
+++ gamejam2025/Scripts/genealogy_tree.gd
@@ -2,6 +2,8 @@ extends Node
 
 
 @onready var info_panel = $info_panel
+@onready var name_label = $info_panel/name_label
+@onready var squirrel_avatar = $info_panel/squirrel_avatar
 
 
 
@@ -17,6 +19,9 @@ func _ready():
 			
 func _on_squirrel_info_requested(sn: SquirrelNode):
 	print("yup")
+	name_label.text = sn.squirrel_name
+	squirrel_avatar.texture = sn.squirrel_avatar
+	print(sn.avatar)
 	info_panel.visible = true
 	
 	
