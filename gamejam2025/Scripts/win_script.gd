b3268455fe2f7bdce4c3dd22e687ff647f5c6026
Cédrik Lampron <cedrik.lampron@usherbrooke.ca>
2025-03-23 09:50:43 -0400

    tree visible and fight fix

--- gamejam2025/Scripts/win_script.gd
+++ gamejam2025/Scripts/win_script.gd
@@ -4,14 +4,23 @@ extends Node
 @onready var button = $button
 @onready var dialog = $dialog
 @onready var name_label = $name
+@onready var genealogy_tree = $"../GenealogyTree"
+
+var defeated_squirrel: SquirrelNode
+
 
 func _ready():
 	button.pressed.connect(_on_button_next_pressed)
 
 func _on_button_next_pressed():
 	print("next")
+	genealogy_tree.info_panel.visible = false
+	if is_instance_valid(defeated_squirrel):
+		defeated_squirrel._on_button2_pressed()
+
 	
 func set_squirrel(squirrel: SquirrelNode) -> void:
+	defeated_squirrel = squirrel
 	$avatar.texture = squirrel.squirrel_avatar
 	$name.text = squirrel.squirrel_name
 	$dialog.text = squirrel.win_text	
