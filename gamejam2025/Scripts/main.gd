7419ab4f2460925e73aaee35740e46796f6a079f
Simon Létourneau <lets0801@usherbrooke.ca>
2025-04-14 19:49:16 -0400

    Link avec LoseScreen

--- gamejam2025/Scripts/main.gd
+++ gamejam2025/Scripts/main.gd
@@ -1,16 +1,11 @@
 extends Node2D
 
-# Main Menu Scene
+#Scenes
 @onready var main_menu_scene: Node2D = $MainMenuScene
-# Tree Scene
 @onready var tree_scene: Node2D = $TreeScene
-# Combat Scene
 @onready var combat_scene: Node2D = $CombatScene
-# Final Win Scene
 @onready var final_win_scene: Node2D = $FinalWinScene
-# Lose Scene
 @onready var lose_scene: Node2D = $LoseScene
-# Credits Scene
 @onready var credits_scene: Node2D = $CreditsScene
 
 @onready var audio_manager: Node2D = $AudioManager
@@ -18,17 +13,7 @@ extends Node2D
 func _ready() -> void:
 	connect_main_menu_signals()
 	connect_credits_signals()
-
-func connect_buttons_signals():
-	#var temp_start_game_button = main_menu_scene.get_node("StartGameButton")
-	#var temp_credits_button = main_menu_scene.get_node("CreditsButton")
-	var temp_back_to_menu_credits = credits_scene.get_node("BackToMenu")
-	var temp_back_to_menu = lose_scene.get_node("BackToMenu")
-	
-	#temp_start_game_button.pressed.connect(_on_start_game_pressed)
-	#temp_credits_button.pressed.connect(_on_credit_pressed)
-	#temp_back_to_menu_credits.pressed.connect(_on_credit_back_to_menu_pressed)
-	#temp_back_to_menu.pressed.connect(_on_credit_back_to_menu_pressed)
+	connect_lose_screen_signals()
 	
 func connect_main_menu_signals():
 	main_menu_scene.connect("start_game_button_pressed", Callable(self, "_on_start_game_pressed"))
@@ -36,6 +21,9 @@ func connect_main_menu_signals():
 
 func connect_credits_signals():
 	credits_scene.connect("back_to_menu_button_pressed", Callable(self, "_on_back_to_menu_pressed"))
+	
+func connect_lose_screen_signals():
+	lose_scene.connect("back_to_menu_button_pressed", Callable(self, "_on_back_to_menu_pressed"))
 	
 func _on_start_game_pressed():
 	main_menu_scene.visible = false
@@ -54,9 +42,7 @@ func enter_tree_scene():
 	tree_scene.visible = true
 	
 	audio_manager.get_node("BattleMusic").stop()
-	audio_manager.get_node("TreeMusic").play() # TODO
-
-	#end_turn_button.visible = false
+	audio_manager.get_node("TreeMusic").play() 
 
 func enter_credits_scene():
 	all_invisible()
@@ -69,8 +55,3 @@ func all_invisible():
 	credits_scene.visible = false
 	final_win_scene.visible = false
 	lose_scene.visible = false
-
-	#card_manager.set_physics_process(false)
-	#card_manager.set_process(false)
-#
-	#end_turn_button.visible = false
